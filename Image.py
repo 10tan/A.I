3d1b41e6ad1c612ab1710503a4ccb759d508e345
Tanmay <kumartanmay109@gmail.com>
2021-01-29 19:06:52 +0530

    Face Detection Files Added

---
+++ Image.py
@@ -0,0 +1,69 @@
+
+# ----------------- Face Detection Code for an Image -----------------
+
+# Steps
+    # 1. Load of Faces
+    # 2. Make them black and white
+    # 3. Create the Algorithms
+
+import cv2
+from random import randrange
+
+# Load pretrained dataset of opencv in xml file  
+face_data = cv2.CascadeClassifier('Data.xml')
+# eye_data = cv2.CascadeClassifier('haarcascade_eye.xml')
+# smile_data = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+# Choose an image to read using opencv  
+# img = cv2.imread('rdj.jpg')
+# img = cv2.imread('nr.jpg')
+# img = cv2.imread('ce.jpg')
+img = cv2.imread('group.jpg')
+# img = cv2.imread('me.jpeg')
+
+# Convert the image to a grayscale image
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Detect Faces
+face_coo = face_data.detectMultiScale(gray_img)
+# eye_coo = eye_data.detectMultiScale(gray_img)
+
+# smile_coo = smile_data.detectMultiScale(gray_img)
+# detectMultiScale it is going to detect the faces in multiscale 
+
+# prints the face Coordinates 
+print(face_coo)
+
+# Draw Rectangle surrounding the face coordinates
+
+# Two Ways 
+
+# [x,y,w,h] = face_coo[0]
+# cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,0), 2)
+
+# For Face 
+for x,y,w,h in face_coo:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (randrange(156,256),randrange(156,256),randrange(156,256)), 2)
+
+# For Eyes 
+# for x,y,w,h in eye_coo:
+#     cv2.rectangle(img, (x,y), (x+w, y+h),(0,255,0), 1)
+
+# For Smile 
+# for x,y,w,h in smile_coo:
+#     cv2.rectangle(img, (x,y), (x+w, y+h),(0,255,0), 1)
+
+# cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+# cv2.rectangle(img, (30,38), (30 + 118,38 + 118), (255,0,0), 4)
+
+#It will create a popup with the image and a name
+cv2.imshow("Face Detector", img)
+
+
+#It will make the popup wait for a while
+cv2.waitKey()
+
+print("Code Completed") 
+
+
+
---
+++ Video.py
@@ -0,0 +1,44 @@
+# Steps
+    # 1. Load of Faces
+    # 2. Make them black and white
+    # 3. Create the Algorithms
+
+import cv2
+from random import randrange
+
+# Load pretrained dataset of opencv in xml file  
+face_data = cv2.CascadeClassifier('Data.xml')
+# eye_data = cv2.CascadeClassifier('haarcascade_eye.xml')
+# smile_data = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+# Choose a video to read using opencv
+
+# For Prerecorded video stored in computer
+# video = cv2.VideoCapture('me.mp4')
+video = cv2.VideoCapture(0)
+
+# Iterate forever through the frames in video 
+while True:
+    # Read the Current Frame 
+    successfull_frame_read, frame = video.read()
+    gray_video = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #Detect Faces
+    face_coo = face_data.detectMultiScale(gray_video)
+
+    # # Two Ways To draw Rectangle over the face
+
+    # # [x,y,w,h] = face_coo[0]
+    # # cv2.rectangle(video, (x,y), (x+w, y+h), (255,255,0), 2)
+
+    for (x,y,w,h) in face_coo:
+        cv2.rectangle(frame, (x,y), (x+w, y+h),(randrange(156,256),randrange(156,256),randrange(156,256)), 4)
+    
+    cv2.imshow("Face Detector ", frame)
+
+    # if key==81 or key==118:
+    #     break
+    cv2.waitKey(1)
+
+# # It uses the webcam connected to the computer
+# # video = cv2.VideoCapture(0)
